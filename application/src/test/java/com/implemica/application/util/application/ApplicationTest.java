c08f21c8ca2c9ea68b2c42a92420d7bfdf2b3290
Petro <ipov.petr.htcones@gmail.com>
2023-02-05 18:11:28 +0200

    Disable swagger sdk

--- application/src/test/java/com/implemica/application/util/application/ApplicationTest.java
+++ application/src/test/java/com/implemica/application/util/application/ApplicationTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 // Test class added ONLY to cover main() invocation not covered by application tests.
 public class ApplicationTest {
     @Test
-
     public void main() {
         CarCatalogApplication.main(new String[] {});
     }
