b1bf6fc7a0e2f65bb8cc29f0e1507add00e5966c
Petro <ipov.petr.htcones@gmail.com>
2023-02-05 14:14:32 +0200

    Push on bitbucket

--- application/src/main/java/com/implemica/application/CarCatalogApplication.java
+++ application/src/main/java/com/implemica/application/CarCatalogApplication.java
@@ -19,6 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaRepositories(basePackages = "com.implemica")
 @EntityScan(basePackages = "com.implemica")
 public class CarCatalogApplication {
+    //push on bitbucket
     public static void main(String[] args) {
         SpringApplication.run(CarCatalogApplication.class, args);
     }
