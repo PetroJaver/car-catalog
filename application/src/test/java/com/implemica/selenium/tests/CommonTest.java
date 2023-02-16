7100ef332927c6189c05c2677962db033463cee6
Petro <ipov.petr.htcones@gmail.com>
2023-02-16 18:13:53 +0200

    Enable selenium test.

--- application/src/test/java/com/implemica/selenium/tests/CommonTest.java
+++ application/src/test/java/com/implemica/selenium/tests/CommonTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 import static com.implemica.selenium.helpers.BaseTestValues.*;
 import static org.junit.Assert.*;
 
-@Ignore
 public class CommonTest extends BaseSeleniumTest {
     private EditCarPage editCarPage = new EditCarPage();
 
